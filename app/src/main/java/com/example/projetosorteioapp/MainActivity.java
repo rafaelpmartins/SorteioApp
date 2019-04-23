5e7cc2fbfb4d183d7e40d90784265962df768c3d
rafaelmartins <Rafael.martins92@etec.sp.gov.br>
2019-04-23 15:11:17 -0300

    Adicionei Função Randomica ao botao

---
+++ app/src/main/java/com/example/projetosorteioapp/MainActivity.java
@@ -0,0 +1,26 @@
+package com.example.projetosorteioapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import java.util.Random;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void sortearNumero(View view) {
+        TextView ResultadoTexto = findViewById(R.id.IdResultadoTexto);
+
+        int Numero = new Random().nextInt(11);
+
+        ResultadoTexto.setText("O Resultado É: " + Numero);
+
+    }
+}
